1ab9dc72e6b0572d809e103f13bed0795226e1cd
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-01-16 06:54:01 +0000

    modify fibfull example

--- examples/fibfull/src/main.rs
+++ examples/fibfull/src/main.rs
@@ -64,91 +64,93 @@ impl<BaseElement: FieldElement> PublicInput<BaseElement> for FibFullPublicInputs
 }
 
 include!("../bindings.rs");
-
+use std::env;
 fn main() {
-    let const_pols = String::from("a");
-    let const_tree = String::from("b");
-    let stark_info_file = String::from("c");
-    let commit_pols = String::from("d");
-    let verkey = String::from("e");
+    let sample_data_dir = env::current_dir().unwrap().join("../sample_data");
+    let const_pols = sample_data_dir.join("fibonacci.const").display().to_string();
+    let const_tree = sample_data_dir.join("fibonacci.consttree").display().to_string();
+    let stark_info_file = sample_data_dir.join("fibonacci.starkinfo.json").display().to_string();
+    let commit_pols = sample_data_dir.join("fibonacci.commit").display().to_string();
+    let verkey = sample_data_dir.join("fibonacci.verkey.json").display().to_string();
+
     ffi::generate_proof(const_pols, const_tree, stark_info_file, commit_pols, verkey);
 
-    // read command-line args
-    let opt = FibFullOptions::from_args();
+    // // read command-line args
+    // let opt = FibFullOptions::from_args();
 
-    // CHECKS
-    // Check if public inputs file exists
-    if !opt.public_inputs.exists() {
-        eprintln!(
-            "Error: Public inputs file '{}' does not exist",
-            opt.public_inputs.display()
-        );
-        std::process::exit(1);
-    }
+    // // CHECKS
+    // // Check if public inputs file exists
+    // if !opt.public_inputs.exists() {
+    //     eprintln!(
+    //         "Error: Public inputs file '{}' does not exist",
+    //         opt.public_inputs.display()
+    //     );
+    //     std::process::exit(1);
+    // }
 
-    // Check if prover settings file exists
-    if !opt.prover_settings.exists() {
-        eprintln!(
-            "Error: Prover settings file '{}' does not exist",
-            opt.prover_settings.display()
-        );
-        std::process::exit(1);
-    }
+    // // Check if prover settings file exists
+    // if !opt.prover_settings.exists() {
+    //     eprintln!(
+    //         "Error: Prover settings file '{}' does not exist",
+    //         opt.prover_settings.display()
+    //     );
+    //     std::process::exit(1);
+    // }
 
-    // Check if output file already exists
-    if opt.output.exists() {
-        eprintln!(
-            "Error: Output file '{}' already exists",
-            opt.output.display()
-        );
-        std::process::exit(1);
-    }
+    // // Check if output file already exists
+    // if opt.output.exists() {
+    //     eprintln!(
+    //         "Error: Output file '{}' already exists",
+    //         opt.output.display()
+    //     );
+    //     std::process::exit(1);
+    // }
 
-    // Create prover
-    // read prover settings file
-    let estark_settings = match std::fs::read_to_string(&opt.prover_settings) {
-        Ok(settings) => ESTARKProverSettings::new(settings),
-        Err(err) => {
-            eprintln!(
-                "Error reading settings file '{}': {}",
-                opt.prover_settings.display(),
-                err
-            );
-            std::process::exit(1);
-        }
-    };
+    // // Create prover
+    // // read prover settings file
+    // let estark_settings = match std::fs::read_to_string(&opt.prover_settings) {
+    //     Ok(settings) => ESTARKProverSettings::new(settings),
+    //     Err(err) => {
+    //         eprintln!(
+    //             "Error reading settings file '{}': {}",
+    //             opt.prover_settings.display(),
+    //             err
+    //         );
+    //         std::process::exit(1);
+    //     }
+    // };
 
-    //read public inputs file
-    let public_inputs = match std::fs::read_to_string(&opt.public_inputs) {
-        Ok(public_inputs) => FibFullPublicInputs::new(public_inputs),
-        Err(err) => {
-            eprintln!(
-                "Error reading public inputs file '{}': {}",
-                opt.public_inputs.display(),
-                err
-            );
-            std::process::exit(1);
-        }
-    };
+    // //read public inputs file
+    // let public_inputs = match std::fs::read_to_string(&opt.public_inputs) {
+    //     Ok(public_inputs) => FibFullPublicInputs::new(public_inputs),
+    //     Err(err) => {
+    //         eprintln!(
+    //             "Error reading public inputs file '{}': {}",
+    //             opt.public_inputs.display(),
+    //             err
+    //         );
+    //         std::process::exit(1);
+    //     }
+    // };
 
-    let options = ProofManOpt {
-        debug: opt._debug,
-        ..ProofManOpt::default()
-    };
+    // let options = ProofManOpt {
+    //     debug: opt._debug,
+    //     ..ProofManOpt::default()
+    // };
 
-    type GoldiLocks = BaseElement;
-    let prover = ESTARKProver::new(estark_settings /* prover_options */);
+    // type GoldiLocks = BaseElement;
+    // let prover = ESTARKProver::new(estark_settings /* prover_options */);
 
-    let executor = Box::new(FibonacciExecutor);
+    // let executor = Box::new(FibonacciExecutor);
 
-    let mut proofman = ProofManager::<GoldiLocks>::new(
-        "examples/fibfull/src/tmp/pilout.ptb",
-        vec![executor],
-        Box::new(prover),
-        options,
-    );
+    // let mut proofman = ProofManager::<GoldiLocks>::new(
+    //     "examples/fibfull/src/tmp/pilout.ptb",
+    //     vec![executor],
+    //     Box::new(prover),
+    //     options,
+    // );
 
-    let now = Instant::now();
-    proofman.prove(Some(Box::new(public_inputs)));
-    info!("Proof generated in {} ms", now.elapsed().as_millis());
+    // let now = Instant::now();
+    // proofman.prove(Some(Box::new(public_inputs)));
+    // info!("Proof generated in {} ms", now.elapsed().as_millis());
 }
