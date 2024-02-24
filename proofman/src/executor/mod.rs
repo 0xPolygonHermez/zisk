9733533208340fa2384f84116c5bc88844151f4b
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-02-24 07:39:35 +0100

    refactor executors to simplify config

--- proofman/src/executor/mod.rs
+++ proofman/src/executor/mod.rs
@@ -1,79 +1,35 @@
 pub mod executors_manager;
 pub mod executors_manager_thread;
 use crate::proof_ctx::ProofCtx;
-use std::any::Any;
 
-// NOTE: config argument is added temporaly while integrating with zkevm-prover, remove when done
 pub trait Executor<T> {
     fn witness_computation(&self, stage_id: u32, proof_ctx: &mut ProofCtx<T>);
 }
 
-pub trait ExecutorConfig: Any {
-    fn as_any(&self) -> &dyn Any;
-}
-
-// pub trait ExecutorBase<T>: Sync {
-//     fn get_name(&self) -> String;
-
-//     fn _witness_computation(
-//         &self,
-//         config: &Box<dyn Config>,
-//         stage_id: u32,
-//         proof_ctx: Arc<RwLock<&mut ProofCtx<T>>>,
-//         tasks: &TasksTable,
-//         tx: SenderB<Message>,
-//         rx: ReceiverB<Message>,
-//     );
-
-//     fn broadcast(&self, tx: &SenderB<Message>, payload: Payload) {
-//         let msg = Message { src: self.get_name(), dst: "*".to_string(), payload };
-//         tx.send(msg);
-//     }
-// }
-
 #[macro_export]
 macro_rules! executor {
-    ($executor_name:ident/*: $base_element:ty*/) => {
-        pub struct $executor_name {
-            config: Option<Box<dyn $crate::executor::ExecutorConfig>>,
-            ptr: std::cell::UnsafeCell<*mut u8>,
-        }
+    ($executor_name:ident) => {
+            executor!($executor_name {});
+    };
 
-        unsafe impl Send for $executor_name {}
-        unsafe impl Sync for $executor_name {}
+    ($executor_name:ident { $( $field:ident : $field_type:ty ),* $(,)? }) => {
+        pub struct $executor_name {
+            ptr: std::cell::UnsafeCell<*mut u8>,
+            $( $field : $field_type ),*
+        }
 
         impl $executor_name {
             fn get_name(&self) -> String {
                 stringify!($executor_name).to_string()
             }
 
-            pub fn new(config: Option<Box<dyn $crate::executor::ExecutorConfig>>) -> Self {
-                $executor_name { config, ptr: std::cell::UnsafeCell::new(std::ptr::null_mut()) }
+            pub fn new($( $field : $field_type ),*) -> Self {
+                $executor_name { ptr: std::cell::UnsafeCell::new(std::ptr::null_mut()), $( $field ),* }
             }
 
-            pub fn from_ptr(config: Option<Box<dyn $crate::executor::ExecutorConfig>>, ptr: *mut u8) -> Self {
-                $executor_name { config, ptr: std::cell::UnsafeCell::new(ptr) }
+            pub fn from_ptr(ptr: *mut u8, $( $field : $field_type ),*) -> Self {
+                $executor_name { ptr: std::cell::UnsafeCell::new(ptr), $( $field ),* }
             }
         }
-
-        // impl $crate::executor::ExecutorBase<$base_element> for $executor_name {
-        //     fn get_name(&self) -> String {
-        //         stringify!($executor_name).to_string()
-        //     }
-
-        //     fn _witness_computation(
-        //         &self,
-        //         config: &Box<dyn $crate::config::Config>,
-        //         stage_id: u32,
-        //         proof_ctx: std::sync::Arc<std::sync::RwLock<&mut $crate::proof_ctx::ProofCtx<$base_element>>>,
-        //         tasks: &$crate::task::TasksTable,
-        //         tx: $crate::channel::SenderB<Message>,
-        //         rx: $crate::channel::ReceiverB<Message>,
-        //     ) {
-        //         self.witness_computation(&config, stage_id, proof_ctx, tasks, &tx, &rx);
-        //         self.broadcast(&tx, $crate::message::Payload::Finished);
-        //         ()
-        //     }
-        // }
     };
 }
