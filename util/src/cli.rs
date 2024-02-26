02f5eea069ad921ce445ac11048eb6e9f80dfe86
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-02-26 17:39:57 +0100

    fmt

--- util/src/cli.rs
+++ util/src/cli.rs
@@ -2,9 +2,7 @@ use sysinfo::System;
 
 pub const PURPLE: &str = "\x1b[35m";
 pub const GREEN: &str = "\x1b[32;1m";
-
 pub const RESET: &str = "\x1b[37;0m";
-
 pub const BOLD: &str = "\x1b[1m";
 
 pub fn print_banner(extended: bool) {
