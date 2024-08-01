180f423025ed362eef00bcb75654ac506dfeb095
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-08-01 11:27:52 +0200

    wip pil-helpers

--- cli/src/commands/pil_helpers.rs
+++ cli/src/commands/pil_helpers.rs
@@ -1,6 +1,6 @@
 // extern crate env_logger;
 use clap::Parser;
-use pilout::pilout_proxy::PilOutProxy;
+use pilout::{pilout::SymbolType, pilout_proxy::PilOutProxy};
 use serde::Serialize;
 use tinytemplate::TinyTemplate;
 use std::{fs, path::PathBuf};
@@ -42,6 +42,13 @@ struct AirGroupsCtx {
 struct AirCtx {
     name: String,
     num_rows: u32,
+    columns: Vec<ColumnCtx>,
+}
+
+#[derive(Debug, Serialize)]
+struct ColumnCtx {
+    name: String,
+    r#type: String,
 }
 
 impl PilHelpersCmd {
@@ -83,8 +90,7 @@ impl PilHelpersCmd {
         let mut constant_subproofs: Vec<(String, usize)> = Vec::new();
         let mut constant_airs: Vec<(String, Vec<usize>, String)> = Vec::new();
 
-        for (index, subproof) in pilout.subproofs.iter().enumerate() {
-            let subproof_id = index;
+        for (subproof_id, subproof) in pilout.subproofs.iter().enumerate() {
             wcctxs.push(AirGroupsCtx {
                 subproof_id,
                 name: subproof.name.as_ref().unwrap().clone().to_case(Case::Pascal),
@@ -92,7 +98,11 @@ impl PilHelpersCmd {
                 airs: subproof
                     .airs
                     .iter()
-                    .map(|air| AirCtx { name: air.name.as_ref().unwrap().clone(), num_rows: air.num_rows.unwrap() })
+                    .map(|air| AirCtx {
+                        name: air.name.as_ref().unwrap().clone(),
+                        num_rows: air.num_rows.unwrap(),
+                        columns: Vec::new(),
+                    })
                     .collect(),
             });
 
@@ -118,6 +128,35 @@ impl PilHelpersCmd {
             }
         }
 
+        // Build columns data for traces
+        for (subproof_id, subproof) in pilout.subproofs.iter().enumerate() {
+            for (air_id, _) in subproof.airs.iter().enumerate() {
+                // Search symbols where subproof_id == subproof_id && air_id == air_id && type == WitnessCol
+                pilout
+                    .symbols
+                    .iter()
+                    .filter(|symbol| {
+                        symbol.subproof_id.is_some()
+                            && symbol.subproof_id.unwrap() == subproof_id as u32
+                            && symbol.air_id.is_some()
+                            && symbol.air_id.unwrap() == air_id as u32
+                            && symbol.stage.is_some()
+                            && symbol.stage.unwrap() == 1
+                            && symbol.r#type == SymbolType::WitnessCol as i32
+                    })
+                    .for_each(|symbol| {
+                        let air = wcctxs[subproof_id].airs.get_mut(air_id).unwrap();
+                        let name = symbol.name.splitn(2, '.').nth(1).unwrap_or(&symbol.name);
+                        let r#type = if symbol.dim == 0 {
+                            "Goldilocks".to_string()
+                        } else {
+                            format!("[Goldilocks; {}]", symbol.lengths[0])
+                        };
+                        air.columns.push(ColumnCtx { name: name.to_owned(), r#type });
+                    });
+            }
+        }
+
         let context = ProofCtx {
             project_name: pilout.name.clone().unwrap().to_case(Case::Pascal),
             pilout_filename: self.pilout.file_name().unwrap().to_str().unwrap().to_string(),
@@ -131,6 +170,7 @@ impl PilHelpersCmd {
         let mut tt = TinyTemplate::new();
         tt.add_template("mod.rs", MOD_RS)?;
         tt.add_template("pilout.rs", include_str!("../../assets/templates/pil_helpers_pilout.rs.tt"))?;
+        tt.add_template("traces.rs", include_str!("../../assets/templates/pil_helpers_trace.rs.tt"))?;
 
         // Write the files
         // --------------------------------------------
@@ -139,6 +179,9 @@ impl PilHelpersCmd {
 
         // Write pilout.rs
         fs::write(pil_helpers_path.join("pilout.rs"), tt.render("pilout.rs", &context)?)?;
+
+        // Write traces.rs
+        fs::write(pil_helpers_path.join("traces.rs"), tt.render("traces.rs", &context)?)?;
 
         Ok(())
     }
--- proofman/src/lib.rs
+++ proofman/src/lib.rs
@@ -1,9 +1,11 @@
+mod buffer_allocator;
 mod planner;
 mod proofman;
 pub mod trace;
 pub mod trace_pol;
 mod wcmanager;
 
+pub use buffer_allocator::*;
 pub use planner::*;
 pub use proofman::*;
 pub use wcmanager::*;
