e615c78d1c62e7542f2920a81f4df25cf7161c3d
Héctor Masip <hmasip@polygon.technology>
2024-08-05 16:45:55 -0400

    WIP

---
+++ test/std/special/intermediate_sums.setup.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const { generateSetupTest } = require("pil2-proofman/test/test_utils.js");
+
+function getSettings() {
+    return {
+        name: "Intermediate-Sums-" + Date.now(),
+        airout: {
+            airoutFilename: path.join(__dirname, 'intermediate_sums.pilout'),
+        },
+        prover: {
+            filename: "./src/lib/provers/stark_fri_prover.js",
+            settings: {
+                default: { blowupFactor: 2 },
+            },
+        },
+    };
+
+}
+
+async function main() {
+    let config = getSettings();
+    await generateSetupTest(config);
+}
+
+if (require.main === module) {
+    main();
+}
