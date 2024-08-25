e43611f60b90f3f4a8ba00ba8c870d861f713254
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-08-25 09:39:06 +0200

    added fibonacci vadcop public inputs

--- examples/fibonacci-vadcop/src/fibonacci_lib.rs
+++ examples/fibonacci-vadcop/src/fibonacci_lib.rs
@@ -1,4 +1,5 @@
-use std::sync::Arc;
+use std::io::Read;
+use std::{fs::File, sync::Arc};
 
 use proofman_common::{ExecutionCtx, ProofCtx, WitnessPilout};
 use p3_field::AbstractField;
@@ -9,32 +10,44 @@ use pil_std_lib::Std;
 
 use std::error::Error;
 use std::path::PathBuf;
+use crate::FibonacciVadcopPublicInputs;
 
 use crate::{FibonacciSquare, Pilout, Module};
 
 pub struct FibonacciVadcop<F> {
     pub wcm: WitnessManager<F>,
+    pub public_inputs_path: PathBuf,
     pub fibonacci: Arc<FibonacciSquare>,
     pub module: Arc<Module>,
     pub std_lib: Arc<Std<F>>,
 }
 
 impl<F: AbstractField + Copy> FibonacciVadcop<F> {
-    pub fn new() -> Self {
+    pub fn new(public_inputs_path: PathBuf) -> Self {
         let mut wcm = WitnessManager::new();
 
         let std_lib = Std::new(&mut wcm);
         let module = Module::new(&mut wcm, std);
         let fibonacci = FibonacciSquare::new(&mut wcm, module.clone());
 
-        FibonacciVadcop { wcm, fibonacci, module, std_lib }
+        FibonacciVadcop { wcm, public_inputs_path, fibonacci, module, std_lib }
     }
 }
 
 impl<F: AbstractField + Copy> WitnessLibrary<F> for FibonacciVadcop<F> {
     fn start_proof(&mut self, pctx: &mut ProofCtx<F>, ectx: &ExecutionCtx, sctx: &SetupCtx) {
-        pctx.public_inputs =
-            vec![25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0]; // TODO: NOT SHOULD BE HARDCODED!
+        let mut file = File::open(&self.public_inputs_path).unwrap();
+
+        if !file.metadata().unwrap().is_file() {
+            panic!("Public inputs file not found");
+        }
+
+        let mut contents = String::new();
+        let _ = file.read_to_string(&mut contents);
+
+        let public_inputs: FibonacciVadcopPublicInputs = serde_json::from_str(&contents).unwrap();
+        pctx.public_inputs = public_inputs.into();
+
         self.wcm.start_proof(pctx, ectx, sctx);
     }
 
@@ -58,7 +71,7 @@ impl<F: AbstractField + Copy> WitnessLibrary<F> for FibonacciVadcop<F> {
 #[no_mangle]
 pub extern "Rust" fn init_library(
     _rom_path: Option<PathBuf>,
-    _public_inputs_path: PathBuf,
+    public_inputs_path: PathBuf,
 ) -> Result<Box<dyn WitnessLibrary<Goldilocks>>, Box<dyn Error>> {
     env_logger::builder()
         .format_timestamp(None)
@@ -66,6 +79,6 @@ pub extern "Rust" fn init_library(
         .format_target(false)
         .filter_level(log::LevelFilter::Trace)
         .init();
-    let fibonacci_witness = FibonacciVadcop::new();
+    let fibonacci_witness = FibonacciVadcop::new(public_inputs_path);
     Ok(Box::new(fibonacci_witness))
 }
