6471e61d5669b51740ea51fe853566d622becc50
zkronos73 <zkronos73@gmail.com>
2024-08-30 17:44:22 +0200

    update pilout and helpers

--- pil/fork_0/src/pil_helpers/pilout.rs
+++ pil/fork_0/src/pil_helpers/pilout.rs
@@ -8,19 +8,27 @@ pub const PILOUT_HASH: &[u8] = b"Zisk-hash";
 
 pub const MAIN_SUBPROOF_ID: &[usize] = &[0];
 
+pub const BINARY_SUBPROOF_ID: &[usize] = &[1];
+
 //AIR CONSTANTS
 
 pub const MAIN_AIR_IDS: &[usize] = &[0];
+
+pub const BINARY_AIR_IDS: &[usize] = &[0];
 
 pub struct Pilout;
 
 impl Pilout {
     pub fn pilout() -> WitnessPilout {
-        let mut pilout = WitnessPilout::new("Zisk", 2, PILOUT_HASH.to_vec());
+        let mut pilout = WitnessPilout::new("Zisk", 1, PILOUT_HASH.to_vec());
 
         let air_group = pilout.add_air_group(Some("Main"));
 
         air_group.add_air(Some("Main"), 2097152);
+
+        let air_group = pilout.add_air_group(Some("Binary"));
+
+        air_group.add_air(Some("Binary"), 2097152);
 
         pilout
     }
