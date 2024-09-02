2bb7f8340842d1dc559bc781ab3bf128f6eb849e
Héctor Masip <hmasip@polygon.technology>
2024-09-02 16:09:27 +0000

    Adding lookup test in rust

---
+++ test/std/lookup/rs/src/lib.rs
@@ -0,0 +1,7 @@
+mod pil_helpers;
+mod lookup_lib;
+mod lookup;
+
+pub use pil_helpers::*;
+pub use lookup_lib::*;
+pub use lookup::*;
---
+++ test/std/lookup/rs/src/lookup_lib.rs
@@ -0,0 +1,65 @@
+use std::{error::Error, sync::Arc, path::PathBuf};
+
+use proofman_common::{ExecutionCtx, ProofCtx, SetupCtx, WitnessPilout};
+use proofman::{WitnessLibrary, WitnessManager};
+use pil_std_lib::Std;
+
+use p3_field::PrimeField;
+use p3_goldilocks::Goldilocks;
+
+use crate::{Lookup, Pilout};
+
+pub struct LookupWitness<F> {
+    pub wcm: WitnessManager<F>,
+    pub lookup: Arc<Lookup<F>>,
+    pub std_lib: Arc<Std<F>>,
+}
+
+impl<F: PrimeField> LookupWitness<F> {
+    pub fn new() -> Self {
+        let mut wcm = WitnessManager::new();
+
+        let std_lib = Std::new(&mut wcm, None);
+        let lookup = Lookup::new(&mut wcm);
+
+        LookupWitness { wcm, lookup, std_lib }
+    }
+}
+
+impl<F: PrimeField> WitnessLibrary<F> for LookupWitness<F> {
+    fn start_proof(&mut self, pctx: &mut ProofCtx<F>, ectx: &ExecutionCtx, sctx: &SetupCtx) {
+        self.wcm.start_proof(pctx, ectx, sctx);
+    }
+
+    fn end_proof(&mut self) {
+        self.wcm.end_proof();
+    }
+
+    fn execute(&self, pctx: &mut ProofCtx<F>, ectx: &mut ExecutionCtx, sctx: &SetupCtx) {
+        // Execute those components that need to be executed
+        self.lookup.execute(pctx, ectx, sctx);
+    }
+
+    fn calculate_witness(&mut self, stage: u32, pctx: &mut ProofCtx<F>, ectx: &ExecutionCtx, sctx: &SetupCtx) {
+        self.wcm.calculate_witness(stage, pctx, ectx, sctx);
+    }
+
+    fn pilout(&self) -> WitnessPilout {
+        Pilout::pilout()
+    }
+}
+
+#[no_mangle]
+pub extern "Rust" fn init_library(
+    _rom_path: Option<PathBuf>,
+    _public_inputs_path: PathBuf,
+) -> Result<Box<dyn WitnessLibrary<Goldilocks>>, Box<dyn Error>> {
+    env_logger::builder()
+        .format_timestamp(None)
+        .format_level(true)
+        .format_target(false)
+        .filter_level(log::LevelFilter::Trace)
+        .init();
+    let lookup_witness = LookupWitness::new();
+    Ok(Box::new(lookup_witness))
+}
