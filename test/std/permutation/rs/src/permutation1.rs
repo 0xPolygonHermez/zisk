7f1ffce8b70d7ea9cb98f4cb8f996bda7139dbb8
Héctor Masip <hmasip@polygon.technology>
2024-09-09 15:29:01 +0000

    Permutation tests passing

--- test/std/permutation/rs/src/permutation1.rs
+++ test/std/permutation/rs/src/permutation1.rs
@@ -108,31 +108,37 @@ where
             )
             .unwrap();
 
+            // TODO: Add the ability to send inputs to permutation2
+            //       and consequently add random selectors
+
             // Assumes
             for i in 0..num_rows {
                 trace[i].a1 = rng.gen();
                 trace[i].b1 = rng.gen();
 
-                trace[i].a2 = rng.gen();
-                trace[i].b2 = rng.gen();
+                trace[i].a2 = F::from_canonical_u8(200);
+                trace[i].b2 = F::from_canonical_u8(201);
 
                 trace[i].a3 = rng.gen();
                 trace[i].b3 = rng.gen();
 
-                trace[i].a4 = rng.gen();
-                trace[i].b4 = rng.gen();
+                trace[i].a4 = F::from_canonical_u8(100);
+                trace[i].b4 = F::from_canonical_u8(101);
 
                 trace[i].sel1 = F::one();
                 trace[i].sel3 = F::one(); // F::from_canonical_u8(rng.gen_range(0..=1));
             }
 
+            // TODO: Add the permutation of indexes
+
             // Proves
             for i in 0..num_rows {
-                let mut index = rng.gen_range(0..num_rows);
+                let index = num_rows-i-1;
+                // let mut index = rng.gen_range(0..num_rows);
                 trace[i].c1 = trace[index].a1;
                 trace[i].d1 = trace[index].b1;
 
-                index = rng.gen_range(0..num_rows);
+                // index = rng.gen_range(0..num_rows);
                 trace[i].c2 = trace[index].a3;
                 trace[i].d2 = trace[index].b3;
 
