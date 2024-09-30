80f3cbd592891e395172c6a62aee8bd86ddc4271
RogerTaule <roger.taulee@gmail.com>
2024-09-30 14:37:24 +0000

    Adding get_hint_field_a method

--- hints/src/hints.rs
+++ hints/src/hints.rs
@@ -183,17 +183,23 @@ impl<F: Clone + Copy + Debug + Display> HintFieldValue<F> {
         }
     }
 }
-
 pub trait GetValue<F: Clone + Copy> {
     fn get(&self, index: usize) -> HashMap<Vec<u64>, HintFieldOutput<F>>;
 }
 
 impl<F: Clone + Copy> GetValue<F> for HintFieldValues<F> {
     fn get(&self, index: usize) -> HashMap<Vec<u64>, HintFieldOutput<F>> {
-        self.values
-            .iter() // Iterate over key-value pairs in the HashMap
-            .map(|(key, value)| (key.clone(), value.get(index))) // Apply get to each value
-            .collect() // Collect results into a HashMap
+        self.values.iter().map(|(key, value)| (key.clone(), value.get(index))).collect()
+    }
+}
+
+pub trait GetValueV<F: Clone + Copy> {
+    fn get(&self, index: usize) -> Vec<HintFieldOutput<F>>;
+}
+
+impl<F: Clone + Copy> GetValueV<F> for Vec<HintFieldValue<F>> {
+    fn get(&self, index: usize) -> Vec<HintFieldOutput<F>> {
+        self.iter().map(|value| value.get(index)).collect()
     }
 }
 
