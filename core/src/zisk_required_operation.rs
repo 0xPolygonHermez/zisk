bae01fa5e22cedd075b6c4e3fc4bf2f2dccf4f56
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2024-10-17 17:05:09 +0000

    remove old structs code

--- core/src/zisk_required_operation.rs
+++ core/src/zisk_required_operation.rs
@@ -16,30 +16,6 @@ pub struct ZiskRequiredMemory {
 }
 
 #[derive(Clone, Default)]
-pub struct ZiskRequiredBinaryBasicTable {
-    pub opcode: u8,
-    pub a: u64,
-    pub b: u64,
-    pub row: u64,
-    pub multiplicity: u64,
-}
-
-#[derive(Clone, Default)]
-pub struct ZiskRequiredBinaryExtensionTable {
-    pub opcode: u8,
-    pub a: u64,
-    pub b: u64,
-    pub offset: u64,
-    pub row: u64,
-    pub multiplicity: u64,
-}
-
-#[derive(Clone, Default)]
-pub struct ZiskRequiredRangeCheck {
-    pub rc: u64,
-}
-
-#[derive(Clone, Default)]
 pub struct ZiskRequired {
     pub arith: Vec<ZiskRequiredOperation>,
     pub binary: Vec<ZiskRequiredOperation>,
