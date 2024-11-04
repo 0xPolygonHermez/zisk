685169a92e235a419c126c7dbcc789c226fa0d54
Héctor Masip <hmasip@polygon.technology>
2024-11-04 18:09:19 +0000

    First version of mem unaligned executor

--- state-machines/mem/src/lib.rs
+++ state-machines/mem/src/lib.rs
@@ -2,8 +2,10 @@ mod mem;
 mod mem_aligned;
 mod mem_traces;
 mod mem_unaligned;
+mod mem_unaligned_rom;
 
 pub use mem::*;
 pub use mem_aligned::*;
 pub use mem_traces::*;
 pub use mem_unaligned::*;
+pub use mem_unaligned_rom::*;
