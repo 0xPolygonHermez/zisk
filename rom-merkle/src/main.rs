e0492d3a7e8fa555ad649229bdc0e0efcd5d0f72
RogerTaule <roger.taulee@gmail.com>
2024-11-19 11:16:07 +0000

    Cargo fmt

--- rom-merkle/src/main.rs
+++ rom-merkle/src/main.rs
@@ -2,38 +2,34 @@ use clap::{Arg, Command};
 use colored::Colorize;
 use p3_goldilocks::Goldilocks;
 use proofman_common::{GlobalInfo, ProofType, SetupCtx};
+use proofman_starks_lib_c::{extend_and_merkelize_custom_commit_c, fri_proof_new_c, starks_new_c};
 use sm_rom::RomSM;
 use stark::StarkBufferAllocator;
+use std::ffi::c_void;
 use std::{path::Path, sync::Arc};
 use sysinfo::System;
-use std::ffi::c_void;
-use proofman_starks_lib_c::{starks_new_c, fri_proof_new_c, extend_and_merkelize_custom_commit_c};
-use zisk_pil::{ ROM_AIR_IDS, ZISK_AIRGROUP_ID };
+use zisk_pil::{ROM_AIR_IDS, ZISK_AIRGROUP_ID};
 
 fn main() {
     let matches = Command::new("ROM Handler")
         .version("1.0")
         .about("Compute the Merkle Root of a ROM file")
         .arg(
-            Arg::new("rom")
-                .long("rom")
-                .value_name("FILE")
-                .help("The ROM file path")
-                .required(true)
+            Arg::new("rom").long("rom").value_name("FILE").help("The ROM file path").required(true),
         )
         .arg(
             Arg::new("proving_key")
                 .long("proving-key")
                 .value_name("FILE")
                 .help("The proving key folder path")
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::new("rom_buffer")
                 .long("rom-buffer")
                 .value_name("FILE")
                 .help("The rom buffer path")
-                .required(true)
+                .required(true),
         )
         .get_matches();
 
@@ -96,7 +92,11 @@ fn main() {
     let p_stark = starks_new_c((&setup.p_setup).into(), std::ptr::null_mut());
     let p_proof = fri_proof_new_c((&setup.p_setup).into());
 
-    match RomSM::<Goldilocks>::compute_trace_rom_buffer(rom_path.to_path_buf(), buffer_allocator, &sctx) {
+    match RomSM::<Goldilocks>::compute_trace_rom_buffer(
+        rom_path.to_path_buf(),
+        buffer_allocator,
+        &sctx,
+    ) {
         Ok((commit_id, buffer_rom)) => {
             extend_and_merkelize_custom_commit_c(
                 p_stark,
--- state-machines/rom/src/rom.rs
+++ state-machines/rom/src/rom.rs
@@ -89,24 +89,31 @@ impl<F: Field> RomSM<F> {
         let mut air_instance =
             AirInstance::new(sctx.clone(), ZISK_AIRGROUP_ID, ROM_AIR_IDS[0], None, prover_buffer);
 
-        match self.wcm.get_ectx().cached_buffers_path.as_ref().and_then(|cached_buffers| cached_buffers.get("rom").cloned()) {
+        match self
+            .wcm
+            .get_ectx()
+            .cached_buffers_path
+            .as_ref()
+            .and_then(|cached_buffers| cached_buffers.get("rom").cloned())
+        {
             Some(buffer_path) => {
-                let (_, _, commit_id) = buffer_allocator.clone()
-                .get_buffer_info_custom_commit(&sctx, ZISK_AIRGROUP_ID, ROM_AIR_IDS[0], "rom")
-                .unwrap_or_else(|err| panic!("Error getting buffer info: {}", err));
+                let (_, _, commit_id) = buffer_allocator
+                    .clone()
+                    .get_buffer_info_custom_commit(&sctx, ZISK_AIRGROUP_ID, ROM_AIR_IDS[0], "rom")
+                    .unwrap_or_else(|err| panic!("Error getting buffer info: {}", err));
 
                 air_instance.set_custom_commit_cached_file(&sctx, commit_id, buffer_path);
             }
             None => {
                 let (commit_id_rom, prover_buffer_rom) =
-                Self::compute_trace_rom(rom, buffer_allocator.clone(), &sctx)?;
-        
+                    Self::compute_trace_rom(rom, buffer_allocator.clone(), &sctx)?;
+
                 air_instance.set_custom_commit_id_buffer(&sctx, prover_buffer_rom, commit_id_rom);
             }
         }
 
         let (commit_id_rom, prover_buffer_rom) =
-        Self::compute_trace_rom(rom, buffer_allocator.clone(), &sctx)?;
+            Self::compute_trace_rom(rom, buffer_allocator.clone(), &sctx)?;
 
         air_instance.set_custom_commit_id_buffer(&sctx, prover_buffer_rom, commit_id_rom);
 
@@ -205,7 +212,7 @@ impl<F: Field> RomSM<F> {
         buffer_allocator: Arc<dyn BufferAllocator<F>>,
         sctx: &SetupCtx<F>,
     ) -> Result<(u64, Vec<F>), Box<dyn Error + Send>> {
-        // Get the ELF file path as a string        
+        // Get the ELF file path as a string
         let elf_filename: String = rom_path.to_str().unwrap().into();
         println!("Proving ROM for ELF file={}", elf_filename);
 
