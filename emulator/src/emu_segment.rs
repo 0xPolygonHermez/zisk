4a7aa52258bcab57d9db0856e155da5d0d78e262
zkronos73 <zkronos73@gmail.com>
2025-02-04 22:33:48 +0000

    WIP fix compilation bugs

--- emulator/src/emu_segment.rs
+++ emulator/src/emu_segment.rs
@@ -12,7 +12,15 @@ impl EmuStartingPoint {
     pub fn new(segment_type: ZiskOperationType, pc: u64, sp: u64, c: u64, step: u64) -> Self {
         Self {
             op_type: segment_type,
-            emu_trace_start: EmuTraceStart { pc, sp, c, step, regs: [0; 32], mem_reads_index: 0 },
+            emu_trace_start: EmuTraceStart {
+                pc,
+                sp,
+                c,
+                step,
+                regs: [0; 32],
+                reg_steps: [0; 32],
+                mem_reads_index: 0,
+            },
         }
     }
 }
