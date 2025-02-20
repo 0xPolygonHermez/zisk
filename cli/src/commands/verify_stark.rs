0db81bc6331e6691de3f0423e329c23e7b170412
RogerTaule <roger.taulee@gmail.com>
2025-02-20 16:03:05 +0000

    Pointing to develop

--- cli/src/commands/verify_stark.rs
+++ cli/src/commands/verify_stark.rs
@@ -8,8 +8,7 @@ use std::{fs::File, path::PathBuf};
 use p3_field::AbstractField;
 use p3_goldilocks::Goldilocks;
 
-use proofman::verify_proof;
-use proofman_starks_lib_c::get_zkin_ptr_c;
+use proofman::verify_proof_from_file;
 
 use crate::ZISK_VERSION_MESSAGE;
 
@@ -46,8 +45,6 @@ impl ZiskVerify {
 
         initialize_logger(self.verbose.into());
 
-        let p_proof = get_zkin_ptr_c(&self.proof.clone());
-
         let publics = if let Some(publics) = &self.public_inputs {
             let mut contents = String::new();
             let mut file = File::open(publics).unwrap();
@@ -70,8 +67,8 @@ impl ZiskVerify {
             None
         };
 
-        let valid = verify_proof::<Goldilocks>(
-            p_proof,
+        let valid = verify_proof_from_file::<Goldilocks>(
+            self.proof.clone(),
             self.stark_info.clone(),
             self.verifier_bin.clone(),
             self.verkey.clone(),
