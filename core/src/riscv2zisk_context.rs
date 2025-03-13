5028b4b28fe84181f9cea82d936ab9c7e8da8aa3
fractasy <fractasy@gmail.com>
2025-03-13 09:00:39 +0000

    Fix atomic ops corner case

--- core/src/riscv2zisk_context.rs
+++ core/src/riscv2zisk_context.rs
@@ -666,8 +666,8 @@ impl Riscv2ZiskContext<'_> {
         if i.rd == i.rs1 {
             if i.rd == 0 {
                 let mut zib = ZiskInstBuilder::new(self.s);
-                zib.src_a("mem", 0, false);
-                zib.src_b("mem", 0, false);
+                zib.src_a("imm", 0, false);
+                zib.src_b("imm", 0, false);
                 zib.op("copyb").unwrap();
                 zib.store("mem", CSR_ADDR as i64 + i.csr as i64, false, false);
                 zib.j(4, 4);
@@ -786,8 +786,8 @@ impl Riscv2ZiskContext<'_> {
         if i.rd == i.rs1 {
             if i.rd == 0 {
                 let mut zib = ZiskInstBuilder::new(self.s);
-                zib.src_a("mem", 0, false);
-                zib.src_b("mem", 0, false);
+                zib.src_a("imm", 0, false);
+                zib.src_b("imm", 0, false);
                 zib.op("copyb").unwrap();
                 zib.j(4, 4);
                 zib.verbose(&format!("{} r{}, 0x{:x}, r{} ## rd=rs=0", i.inst, i.rd, i.csr, i.rs1));
@@ -916,8 +916,8 @@ impl Riscv2ZiskContext<'_> {
         if i.rd == i.rs1 {
             if i.rd == 0 {
                 let mut zib = ZiskInstBuilder::new(self.s);
-                zib.src_a("mem", 0, false);
-                zib.src_b("mem", 0, false);
+                zib.src_a("imm", 0, false);
+                zib.src_b("imm", 0, false);
                 zib.op("copyb").unwrap();
                 zib.j(4, 4);
                 zib.verbose(&format!("{} r{}, 0x{:x}, r{} ## rd=rs=0", i.inst, i.rd, i.csr, i.rs1));
