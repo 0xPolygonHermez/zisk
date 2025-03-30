1ebcf83b53f77c40d307ac3eb504f039449ff573
zkronos73 <zkronos73@gmail.com>
2025-03-30 23:08:33 +0000

    fix clippy errors

--- ziskos/entrypoint/src/zisklib/fcalls_impl/secp256k1_fp_inv.rs
+++ ziskos/entrypoint/src/zisklib/fcalls_impl/secp256k1_fp_inv.rs
@@ -2,7 +2,7 @@ use lib_c::secp256k1_fp_inv_c;
 
 pub fn secp256k1_fp_inv(params: &[u64], results: &mut [u64]) -> i64 {
     // Perform the inversion
-    let res_c_call = secp256k1_fp_inv_c(&params, results);
+    let res_c_call = secp256k1_fp_inv_c(params, results);
     if res_c_call == 0 {
         4
     } else {
--- ziskos/entrypoint/src/zisklib/fcalls_impl/secp256k1_fp_sqrt.rs
+++ ziskos/entrypoint/src/zisklib/fcalls_impl/secp256k1_fp_sqrt.rs
@@ -4,7 +4,7 @@ pub fn secp256k1_fp_sqrt(params: &[u64], results: &mut [u64]) -> i64 {
     let parity = params[4];
 
     // Perform the inversion
-    let res_c_call = secp256k1_fp_parity_sqrt_c(&params, parity, results);
+    let res_c_call = secp256k1_fp_parity_sqrt_c(params, parity, results);
     if res_c_call == 0 {
         5
     } else {
