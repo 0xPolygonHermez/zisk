7a5a334ca8423b9906f8dfbd824ffd00010ec8ba
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-03-30 19:29:48 +0000

    move rom counting to witness computation phase

--- emulator/src/emu.rs
+++ emulator/src/emu.rs
@@ -1536,14 +1536,6 @@ impl<'a> Emu<'a> {
             }
         }
 
-        // Get rom bus data
-        let rom_payload = RomBusData::from_instruction(instruction, &self.ctx.inst_ctx);
-
-        // Write rom bus data to rom bus
-        data_bus.write_to_bus(ROM_BUS_ID, &rom_payload);
-
-        // let finished = inst_observer.on_instruction(instruction, &self.ctx.inst_ctx);
-
         // #[cfg(feature = "sp")]
         // self.set_sp(instruction);
         self.set_pc(instruction);
@@ -1646,6 +1638,12 @@ impl<'a> Emu<'a> {
                 data_bus.write_to_bus(OPERATION_BUS_ID, &data);
             }
         }
+
+        // Get rom bus data
+        let rom_payload = RomBusData::from_instruction(instruction, &self.ctx.inst_ctx);
+
+        // Write rom bus data to rom bus
+        data_bus.write_to_bus(ROM_BUS_ID, &rom_payload);
 
         // #[cfg(feature = "sp")]
         // self.set_sp(instruction);
--- state-machines/rom/src/rom_instance.rs
+++ state-machines/rom/src/rom_instance.rs
@@ -2,13 +2,16 @@
 //!
 //! It is responsible for computing witnesses for ROM-related execution plans,
 
-use std::sync::Arc;
+use std::sync::{atomic::AtomicU32, Arc};
 
-use crate::RomSM;
-use data_bus::PayloadType;
+use crate::{rom_counter::RomCounter, RomSM};
+use data_bus::{BusDevice, BusId, PayloadType, ROM_BUS_ID};
 use p3_field::PrimeField;
 use proofman_common::{AirInstance, ProofCtx, SetupCtx};
-use sm_common::{BusDeviceWrapper, CheckPoint, Instance, InstanceCtx, InstanceType};
+use sm_common::{
+    BusDeviceWrapper, CheckPoint, CounterStats, Instance, InstanceCtx, InstanceType, Metrics,
+};
+use zisk_common::ChunkId;
 use zisk_core::ZiskRom;
 
 /// The `RomInstance` struct represents an instance to perform the witness computations for
@@ -22,6 +25,12 @@ pub struct RomInstance {
 
     /// The instance context.
     ictx: InstanceCtx,
+
+    /// Shared biod instruction counter for monitoring ROM operations.
+    bios_inst_count: Arc<Vec<AtomicU32>>,
+
+    /// Shared program instruction counter for monitoring ROM operations.
+    prog_inst_count: Arc<Vec<AtomicU32>>,
 }
 
 impl RomInstance {
@@ -33,8 +42,13 @@ impl RomInstance {
     ///
     /// # Returns
     /// A new `RomInstance` instance initialized with the provided ROM and context.
-    pub fn new(zisk_rom: Arc<ZiskRom>, ictx: InstanceCtx) -> Self {
-        Self { zisk_rom, ictx }
+    pub fn new(
+        zisk_rom: Arc<ZiskRom>,
+        ictx: InstanceCtx,
+        bios_inst_count: Arc<Vec<AtomicU32>>,
+        prog_inst_count: Arc<Vec<AtomicU32>>,
+    ) -> Self {
+        Self { zisk_rom, ictx, bios_inst_count, prog_inst_count }
     }
 }
 
@@ -56,9 +70,23 @@ impl<F: PrimeField> Instance<F> for RomInstance {
         &mut self,
         _pctx: &ProofCtx<F>,
         _sctx: &SetupCtx<F>,
-        _collectors: Vec<(usize, Box<BusDeviceWrapper<PayloadType>>)>,
+        collectors: Vec<(usize, Box<BusDeviceWrapper<PayloadType>>)>,
     ) -> Option<AirInstance<F>> {
-        Some(RomSM::compute_witness(&self.zisk_rom, &self.ictx.plan))
+        let collectors: Vec<_> = collectors
+            .into_iter()
+            .map(|(_, mut collector)| {
+                collector.detach_device().as_any().downcast::<RomCollector>().unwrap()
+            })
+            .collect();
+
+        let mut counter_stats_total =
+            CounterStats::new(self.bios_inst_count.clone(), self.prog_inst_count.clone());
+
+        for collector in collectors {
+            counter_stats_total += &collector.rom_counter.counter_stats;
+        }
+
+        Some(RomSM::compute_witness(&self.zisk_rom, &counter_stats_total))
     }
 
     /// Retrieves the checkpoint associated with this instance.
@@ -75,5 +103,72 @@ impl<F: PrimeField> Instance<F> for RomInstance {
     /// An `InstanceType` representing the type of this instance (`InstanceType::Instance`).
     fn instance_type(&self) -> InstanceType {
         InstanceType::Instance
+    }
+
+    /// Builds an input collector for the instance.
+    ///
+    /// # Arguments
+    /// * `chunk_id` - The chunk ID associated with the input collector.
+    ///
+    /// # Returns
+    /// An `Option` containing the input collector for the instance.
+    fn build_inputs_collector(
+        &self,
+        _chunk_id: ChunkId,
+    ) -> Option<Box<dyn BusDevice<PayloadType>>> {
+        Some(Box::new(RomCollector::new(
+            self.bios_inst_count.clone(),
+            self.prog_inst_count.clone(),
+        )))
+    }
+}
+
+pub struct RomCollector {
+    /// Execution statistics counter for ROM instructions.
+    pub rom_counter: RomCounter,
+}
+
+impl RomCollector {
+    /// Creates a new instance of `RomCounter`.
+    ///
+    /// # Returns
+    /// A new `RomCounter` instance.
+    pub fn new(bios_inst_count: Arc<Vec<AtomicU32>>, prog_inst_count: Arc<Vec<AtomicU32>>) -> Self {
+        let rom_counter = RomCounter::new(bios_inst_count, prog_inst_count);
+        Self { rom_counter }
+    }
+}
+
+impl BusDevice<u64> for RomCollector {
+    /// Processes data received on the bus, updating ROM metrics.
+    ///
+    /// # Arguments
+    /// * `bus_id` - The ID of the bus sending the data.
+    /// * `data` - The data received from the bus.
+    ///
+    /// # Returns
+    /// An optional vector of tuples where:
+    /// - The first element is the bus ID.
+    /// - The second element is always empty indicating there are no derived inputs.
+    #[inline]
+    fn process_data(&mut self, bus_id: &BusId, data: &[u64]) -> Option<Vec<(BusId, Vec<u64>)>> {
+        debug_assert!(*bus_id == ROM_BUS_ID);
+
+        self.rom_counter.measure(data);
+
+        None
+    }
+
+    /// Returns the bus IDs associated with this counter.
+    ///
+    /// # Returns
+    /// A vector containing the connected bus ID.
+    fn bus_id(&self) -> Vec<BusId> {
+        vec![ROM_BUS_ID]
+    }
+
+    /// Provides a dynamic reference for downcasting purposes.
+    fn as_any(self: Box<Self>) -> Box<dyn std::any::Any> {
+        self
     }
 }
