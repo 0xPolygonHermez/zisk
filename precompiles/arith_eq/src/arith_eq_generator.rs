a35d330d00e8751033dd8b41c0d7c0043556b06b
hecmas <hmasip@polygon.technology>
2025-05-15 09:17:08 +0000

    New 10 equation added

--- precompiles/arith_eq/src/arith_eq_generator.rs
+++ precompiles/arith_eq/src/arith_eq_generator.rs
@@ -116,4 +116,176 @@ fn main() {
 
     let pil_file = pil_code_path.join("secp256k1_y3.pil");
     eq.generate_pil_code_to_file("eq_secp256k1_y3", pil_file.to_str().unwrap());
+
+    // BN254
+
+    // s - different points
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "s*x2-s*x1-y2+y1-p*q0+p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x80000000000000000000000000000000000000000000000000000000000000000"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_point_add.rs");
+    eq.generate_rust_code_to_file("Bn254PointAdd", "x1,y1,x2,y2,s,q0", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_point_add.pil");
+    eq.generate_pil_code_to_file("eq_bn254_point_add", pil_file.to_str().unwrap());
+
+    // s - duplicate points
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "2*s*y1-3*x1*x1+p*q0-p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x100000000000000000000000000000000000000000000000000000000000000000"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_point_dbl.rs");
+    eq.generate_rust_code_to_file("Bn254PointDbl", "x1,y1,s,q0", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_point_dbl.pil");
+    eq.generate_pil_code_to_file("eq_bn254_point_dbl", pil_file.to_str().unwrap());
+
+    // x3
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "s*s-x1-x2-x3-p*q1+p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x10"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_point_x3.rs");
+    eq.generate_rust_code_to_file("Bn254PointX3", "x1,x2,x3,s,q1", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_point_x3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_point_x3", pil_file.to_str().unwrap());
+
+    // y3
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "s*x1-s*x3-y1-y3+p*q2-p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x80000000000000000000000000000000000000000000000000000000000000000"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_point_y3.rs");
+    eq.generate_rust_code_to_file("Bn254PointY3", "x1,y1,x3,y3,s,q2", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_point_y3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_point_y3", pil_file.to_str().unwrap());
+
+    // x3 - complex addition
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "x1+x2-x3-p*q1+p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x8"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_add_x3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexAddX3", "x1,x2,x3,q1", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_add_x3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_add_x3", pil_file.to_str().unwrap());
+
+    // y3 - complex addition
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "y1+y2-y3-p*q2+p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x8"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_add_y3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexAddY3", "y1,y2,y3,q2", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_add_y3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_add_y3", pil_file.to_str().unwrap());
+
+    // x3 - complex subtraction
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "x1-x2-x3+p*q1-p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x8"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_sub_x3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexSubX3", "x1,x2,x3,q1", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_sub_x3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_sub_x3", pil_file.to_str().unwrap());
+
+    // y3 - complex subtraction
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "y1-y2-y3+p*q2-p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x8"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_sub_y3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexSubY3", "y1,y2,y3,q2", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_sub_y3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_sub_y3", pil_file.to_str().unwrap());
+
+    // x3 - complex multiplication
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "x1*x2-y1*y2-x3+p*q1-p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x80000000000000000000000000000000000000000000000000000000000000000"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_mul_x3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexMulX3", "x1,y1,x2,y2,x3,q1", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_mul_x3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_mul_x3", pil_file.to_str().unwrap());
+
+    // y3 - complex multiplication
+
+    let mut eq = Equation::new(&config);
+    eq.parse(
+        "y1*x2+x1*y2-y3-p*q2+p*offset",
+        &[
+            ("p", "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47"),
+            ("offset", "0x8"),
+        ],
+    );
+
+    let rust_file = rust_code_path.join("bn254_complex_mul_y3.rs");
+    eq.generate_rust_code_to_file("Bn254ComplexMulY3", "x1,y1,x2,y2,y3,q2", rust_file.to_str().unwrap());
+
+    let pil_file = pil_code_path.join("bn254_complex_mul_y3.pil");
+    eq.generate_pil_code_to_file("eq_bn254_complex_mul_y3", pil_file.to_str().unwrap());
 }
