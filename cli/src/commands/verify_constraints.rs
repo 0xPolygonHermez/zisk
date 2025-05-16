36487a9bb3c213fc790a2c2bb663792f7a4af0dd
roger.taule <rtaule@polygon.technology>
2025-05-16 08:48:13 +0000

    Changes interface

--- cli/src/commands/verify_constraints.rs
+++ cli/src/commands/verify_constraints.rs
@@ -6,7 +6,7 @@ use libloading::{Library, Symbol};
 use log::info;
 use p3_goldilocks::Goldilocks;
 use proofman::ProofMan;
-use proofman_common::{initialize_logger, json_to_debug_instances_map, DebugInfo, ProofOptions};
+use proofman_common::{initialize_logger, json_to_debug_instances_map, DebugInfo, ParamsGPU};
 use rom_setup::{
     gen_elf_hash, get_elf_bin_file_path, get_elf_data_hash, get_rom_blowup_factor,
     DEFAULT_CACHE_PATH,
@@ -159,7 +159,10 @@ impl ZiskVerifyConstraints {
         let proofman = ProofMan::<Goldilocks>::new(
             self.get_proving_key(),
             custom_commits_map,
-            ProofOptions::new(true, self.verbose.into(), false, false, false, false, debug_info),
+            true,
+            false,
+            false,
+            ParamsGPU::default(),
         )
         .expect("Failed to initialize proofman");
 
@@ -181,7 +184,7 @@ impl ZiskVerifyConstraints {
                 proofman.register_witness(&mut *witness_lib);
 
                 proofman
-                    .verify_proof_constraints_from_lib(self.input.clone())
+                    .verify_proof_constraints_from_lib(self.input.clone(), &debug_info)
                     .map_err(|e| anyhow::anyhow!("Error generating proof: {}", e))?;
             }
         };
