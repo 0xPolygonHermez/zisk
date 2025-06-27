b3b9be17435228c1a08ac0ed73b70f6d12862a74
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-06-27 05:49:04 +0000

    fix server cli

--- cli/src/commands/common.rs
+++ cli/src/commands/common.rs
@@ -6,18 +6,13 @@ use std::env;
 use std::fmt::Display;
 use std::path::PathBuf;
 use std::str::FromStr;
+#[cfg(distributed)]
+use zisk_common::MpiContext;
 
 #[derive(Parser, Debug, Clone, ValueEnum)]
 pub enum Field {
     Goldilocks,
     // Add other variants here as needed
-}
-
-pub struct MpiContext {
-    #[cfg(distributed)]
-    pub universe: mpi::environment::Universe,
-    pub world_rank: i32,
-    pub local_rank: i32,
 }
 
 impl FromStr for Field {
