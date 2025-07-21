0097f3e9e3b2697019cf7d97255a3b998ca5669b
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-07-21 07:07:19 +0000

    improve data_bus documentation

--- data-bus/src/data_bus.rs
+++ data-bus/src/data_bus.rs
@@ -36,7 +36,7 @@ pub trait DataBusTrait<D, T> {
 ///   trait.
 pub struct DataBus<D, BD: BusDevice<D>> {
     /// List of devices connected to the bus.
-    pub devices: Vec<(Option<usize>, BD)>,
+    devices: Vec<(Option<usize>, BD)>,
 
     /// Mapping from `BusId` to indices of devices listening to that ID.
     devices_bus_id_map: Vec<Vec<usize>>,
@@ -44,8 +44,10 @@ pub struct DataBus<D, BD: BusDevice<D>> {
     /// Queue of pending data transfers to be processed.
     pending_transfers: VecDeque<(BusId, Vec<D>)>,
 
+    /// Indices of devices that are connected to the bus but without a specific instance.
     none_devices: Vec<usize>,
 
+    /// The number of active devices currently connected to the bus.
     active_devices: usize,
 }
 
@@ -71,7 +73,7 @@ impl<D, BD: BusDevice<D>> DataBus<D, BD> {
     /// Connects a device to the bus with specific `BusId` subscriptions.
     ///
     /// # Arguments
-    /// * `bus_ids` - A vector of `BusId` values the device subscribes to.
+    /// * `instance_idx` - An optional index for the device instance.
     /// * `bus_device` - The device to be added to the bus.
     pub fn connect_device(&mut self, instance_idx: Option<usize>, bus_device: Option<BD>) {
         if let Some(bus_device) = bus_device {
@@ -83,7 +85,6 @@ impl<D, BD: BusDevice<D>> DataBus<D, BD> {
             for bus_id in bus_ids {
                 self.devices_bus_id_map[*bus_id].push(device_idx);
             }
-
             self.active_devices += 1;
         } else {
             self.none_devices.push(self.devices.len());
@@ -95,7 +96,6 @@ impl<D, BD: BusDevice<D>> DataBus<D, BD> {
     /// # Arguments
     /// * `bus_id` - The ID of the bus to route the data to.
     /// * `payload` - A reference to the data payload being routed.
-    /// * `pending` – A queue of pending bus operations used to send derived inputs.
     ///
     /// # Returns
     /// A boolean indicating whether the program should continue execution or terminate.
@@ -107,11 +107,13 @@ impl<D, BD: BusDevice<D>> DataBus<D, BD> {
 
         while i < devices_idx.len() {
             let device_idx = devices_idx[i];
+            // When a device returns false, it indicates that it has finished its work and should be disabled.
             if !self.devices[device_idx].1.process_data(
                 &bus_id,
                 payload,
                 &mut self.pending_transfers,
             ) {
+                // Remove the device from the bus and update the mapping.
                 devices_idx.swap_remove(i);
                 self.active_devices -= 1;
             } else {
@@ -129,6 +131,16 @@ impl<D, BD: BusDevice<D>> DataBus<D, BD> {
 }
 
 impl<D, BD: BusDevice<D>> DataBusTrait<D, BD> for DataBus<D, BD> {
+    /// Writes data to the bus and processes it through the registered devices.
+    /// 
+    /// # Arguments
+    /// * `bus_id` - The ID of the bus receiving the data.
+    /// * `payload` - The data payload to be sent.
+    /// * `pending` – A queue of pending bus operations used to send derived inputs.
+    /// 
+    /// # Returns
+    /// A boolean indicating whether the program should continue execution or terminate.
+    /// Returns `true` to continue execution, `false` to stop.
     #[inline(always)]
     fn write_to_bus(&mut self, bus_id: BusId, payload: &[D]) -> bool {
         self.route_data(bus_id, payload);
@@ -140,12 +152,20 @@ impl<D, BD: BusDevice<D>> DataBusTrait<D, BD> for DataBus<D, BD> {
         self.active_devices > 0
     }
 
+    /// Called when the bus is closed, allowing devices to perform any necessary cleanup.
     fn on_close(&mut self) {
         for device in &mut self.devices {
             device.1.on_close();
         }
     }
 
+    /// Converts the bus into a vector of devices, optionally executing their close operations.
+    /// 
+    /// # Arguments
+    /// * `execute_on_close` - If true, calls the `on_close` method on each device.
+    /// 
+    //// # Returns
+    /// A vector of tuples containing the device instance index and the device itself.
     fn into_devices(self, execute_on_close: bool) -> Vec<(Option<usize>, Option<BD>)> {
         let total_len = self.devices.len() + self.none_devices.len();
         let mut result = Vec::with_capacity(total_len);
