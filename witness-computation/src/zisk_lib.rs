e9e6bba0a27b9ce597e468718e23606d07e45b57
RogerTaule <roger.taulee@gmail.com>
2025-07-30 17:15:59 +0000

    Proofman to 0.11.0

--- witness-computation/src/zisk_lib.rs
+++ witness-computation/src/zisk_lib.rs
@@ -79,7 +79,7 @@ impl<F: PrimeField64> WitnessLibrary<F> for WitnessLib<F> {
     ///
     /// # Panics
     /// Panics if the `Riscv2zisk` conversion fails or if required paths cannot be resolved.
-    fn register_witness(&mut self, wcm: Arc<WitnessManager<F>>) {
+    fn register_witness(&mut self, wcm: &WitnessManager<F>) {
         // Step 1: Create an instance of the RISCV -> ZisK program converter
         let rv2zk = Riscv2zisk::new(self.elf_path.display().to_string());
 
