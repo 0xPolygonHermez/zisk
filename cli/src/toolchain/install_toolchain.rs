59412f83327f1e7402d17dd2048698837910188b
Fran Segovia <agnusmor@gmail.com>
2025-08-11 20:30:07 +0000

    Fix fmt

--- cli/src/toolchain/install_toolchain.rs
+++ cli/src/toolchain/install_toolchain.rs
@@ -13,9 +13,7 @@ use std::{
 #[cfg(target_family = "unix")]
 use std::os::unix::fs::PermissionsExt;
 
-use crate::{
-    get_target, get_toolchain_download_url, is_supported_target, RUSTUP_TOOLCHAIN_NAME,
-};
+use crate::{get_target, get_toolchain_download_url, is_supported_target, url_exists, RUSTUP_TOOLCHAIN_NAME};
 
 #[derive(Parser)]
 #[command(name = "install-toolchain", about = "Install the cargo-zisk toolchain.")]
