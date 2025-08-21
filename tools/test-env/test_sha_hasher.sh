46d3a314bc681d98e6eb4ee6f86506fd20e6aa78
agnusmor <agnusmor@gmail.com>
2025-08-21 12:56:37 +0200

    Enable prove sha_hasher for macos

--- tools/test-env/test_sha_hasher.sh
+++ tools/test-env/test_sha_hasher.sh
@@ -17,9 +17,7 @@ main() {
         total_steps=10
     fi
 
-    if [[ "${PLATFORM}" == "linux" ]]; then
-        is_proving_key_installed || return 1
-    fi   
+    is_proving_key_installed || return 1
 
     step "Loading environment variables..."
     # Load environment variables from .env file
@@ -57,46 +55,44 @@ main() {
         return 1
     fi
 
-    if [[ "${PLATFORM}" == "linux" ]]; then
-        step "Generating program setup..."
-        ensure cargo-zisk rom-setup -e "$ELF_PATH" 2>&1 | tee romsetup_output.log || return 1
-        if ! grep -F "ROM setup successfully completed" romsetup_output.log; then
-            err "program setup failed"
+    step "Generating program setup..."
+    ensure cargo-zisk rom-setup -e "$ELF_PATH" 2>&1 | tee romsetup_output.log || return 1
+    if ! grep -F "ROM setup successfully completed" romsetup_output.log; then
+        err "program setup failed"
+        return 1
+    fi
+
+    step "Verifying constraints..."
+    if [[ "${BUILD_GPU}" == "1" ]]; then
+        warn "Skipping verify constraints step for GPU mode (not supported yet)"
+    else    
+        ensure cargo-zisk verify-constraints -e "$ELF_PATH" -i "$INPUT_BIN" 2>&1 | tee constraints_output.log || return 1
+        if ! grep -F "All global constraints were successfully verified" constraints_output.log; then
+            err "verify constraints failed"
+            return 1
+        fi
+    fi
+
+    if [[ "${DISABLE_PROVE}" != "1" ]]; then
+        step "Generating proof..."
+        MPI_CMD=""
+        # If ZISK_GHA is set, use mpirun command for distributed proving to prove it faster and reduce GHA time
+        if is_gha; then
+            # Build mpi command
+            info "Using mpirun for distributed proving"
+            MPI_CMD="mpirun --allow-run-as-root --bind-to none -np $DISTRIBUTED_PROCESSES -x OMP_NUM_THREADS=$DISTRIBUTED_THREADS -x RAYON_NUM_THREADS=$DISTRIBUTED_THREADS"
+        fi
+        ensure $MPI_CMD cargo-zisk prove -e "$ELF_PATH" -i "$INPUT_BIN" -o proof $PROVE_FLAGS 2>&1 | tee prove_output.log || return 1
+        if ! grep -F "Vadcop Final proof was verified" prove_output.log; then
+            err "prove program failed"
             return 1
         fi
 
-        step "Verifying constraints..."
-        if [[ "${BUILD_GPU}" == "1" ]]; then
-            warn "Skipping verify constraints step for GPU mode (not supported yet)"
-        else    
-            ensure cargo-zisk verify-constraints -e "$ELF_PATH" -i "$INPUT_BIN" 2>&1 | tee constraints_output.log || return 1
-            if ! grep -F "All global constraints were successfully verified" constraints_output.log; then
-                err "verify constraints failed"
-                return 1
-            fi
-        fi
-
-        if [[ "${DISABLE_PROVE}" != "1" ]]; then
-            step "Generating proof..."
-            MPI_CMD=""
-            # If ZISK_GHA is set, use mpirun command for distributed proving to prove it faster and reduce GHA time
-            if is_gha; then
-                # Build mpi command
-                info "Using mpirun for distributed proving"
-                MPI_CMD="mpirun --allow-run-as-root --bind-to none -np $DISTRIBUTED_PROCESSES -x OMP_NUM_THREADS=$DISTRIBUTED_THREADS -x RAYON_NUM_THREADS=$DISTRIBUTED_THREADS"
-            fi
-            ensure $MPI_CMD cargo-zisk prove -e "$ELF_PATH" -i "$INPUT_BIN" -o proof $PROVE_FLAGS 2>&1 | tee prove_output.log || return 1
-            if ! grep -F "Vadcop Final proof was verified" prove_output.log; then
-                err "prove program failed"
-                return 1
-            fi
-
-            step "Verifying proof..."
-            ensure cargo-zisk verify -p ./proof/vadcop_final_proof.bin 2>&1 | tee verify_output.log || return 1
-            if ! grep -F "Stark proof was verified" verify_output.log; then
-                err "verify proof failed"
-                return 1
-            fi
+        step "Verifying proof..."
+        ensure cargo-zisk verify -p ./proof/vadcop_final_proof.bin 2>&1 | tee verify_output.log || return 1
+        if ! grep -F "Stark proof was verified" verify_output.log; then
+            err "verify proof failed"
+            return 1
         fi
     fi
 
