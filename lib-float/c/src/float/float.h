376abbbb35d0a08339fc0868a57c8564d30f2e44
fractasy <fractasy@gmail.com>
2025-09-10 13:51:28 +0200

    Make main calculate fadd.d f1, f2, f3

--- lib-float/c/src/float/float.h
+++ lib-float/c/src/float/float.h
@@ -7,6 +7,50 @@
 extern "C" {
 #endif
 
+#define uint64_t unsigned long long
+
+// System address where the floating-point registers are mapped
+const uint64_t SYS_ADDR = 0xa0000000;
+const uint64_t REG_FIRST = SYS_ADDR;
+const uint64_t FREG_OFFSET = 40;
+const uint64_t FREG_FIRST = SYS_ADDR + FREG_OFFSET * 8;
+const uint64_t FREG_F0 = FREG_FIRST;
+const uint64_t FREG_CSR = FREG_FIRST + 32 * 8; // Floating-point control and status register (fcsr)
+const uint64_t FREG_INST = FREG_FIRST + 33 * 8; // Floating-point instruction register (finst)
+static uint64_t myvalue = 0x3ff3333333333333; // 1.7
+extern uint64_t * regs;
+extern uint64_t * fregs;
+
+// Negate a float by flipping its sign bit(s)
+const uint64_t F64_SIGN_BIT_MASK = 0x8000000000000000;
+const uint64_t F32_SIGN_BIT_MASK = 0xFFFFFFFF80000000;
+#define NEG64(x) ((x) ^ F64_SIGN_BIT_MASK)
+#define NEG32(x) ((x) ^ F32_SIGN_BIT_MASK)
+const uint64_t F64_EXPONENT_MASK = 0x7FF0000000000000;
+const uint64_t F32_EXPONENT_MASK = 0x7F800000;
+const uint64_t F64_MANTISSA_MASK = 0x000FFFFFFFFFFFFF;
+const uint64_t F32_MANTISSA_MASK = 0x007FFFFF;
+const uint64_t F64_QUIET_NAN_MASK = 0x0008000000000000;
+const uint64_t F32_QUIET_NAN_MASK = 0x00400000;
+
+// Plus and minus infinity in IEEE 754 format
+const uint64_t F64_MINUS_INFINITE = 0xFFF0000000000000;
+const uint64_t F64_PLUS_INFINITE = 0x7FF0000000000000;
+const uint32_t F32_MINUS_INFINITE = 0xFF800000;
+const uint32_t F32_PLUS_INFINITE = 0x7F800000;
+
+// Plus and minus zero in IEEE 754 format
+const uint64_t F64_MINUS_ZERO = 0x8000000000000000;
+const uint64_t F64_PLUS_ZERO = 0x0000000000000000;
+const uint32_t F32_MINUS_ZERO = 0x80000000;
+const uint32_t F32_PLUS_ZERO = 0x00000000;
+
+// 1.0 and 0.0 in IEEE 754 format
+const uint64_t F64_ONE = 0x3FF0000000000000;
+const uint64_t F32_ONE = 0x3F800000;
+const uint64_t F64_ZERO = 0x0000000000000000;
+const uint32_t F32_ZERO = 0x00000000;
+
 void zisk_float (void);
 
 #ifdef __cplusplus
--- lib-float/c/src/main.cpp
+++ lib-float/c/src/main.cpp
@@ -1,80 +1,17 @@
-//#include <stdio.h>
-//#include <sys/time.h>
-//#include <stdlib.h>
-//#include <cstdint>
-//#include <cstring>
 #include "float/float.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// uint64_t TimeDiff(const struct timeval &startTime, const struct timeval &endTime)
-// {
-//     struct timeval diff;
-
-//     // Calculate the time difference
-//     diff.tv_sec = endTime.tv_sec - startTime.tv_sec;
-//     if (endTime.tv_usec >= startTime.tv_usec)
-//     {
-//         diff.tv_usec = endTime.tv_usec - startTime.tv_usec;
-//     }
-//     else if (diff.tv_sec > 0)
-//     {
-//         diff.tv_usec = 1000000 + endTime.tv_usec - startTime.tv_usec;
-//         diff.tv_sec--;
-//     }
-//     else
-//     {
-//         // gettimeofday() can go backwards under some circumstances: NTP, multithread...
-//         return 0;
-//     }
-
-//     // Return the total number of us
-//     return diff.tv_usec + 1000000 * diff.tv_sec;
-// }
-
-// uint64_t TimeDiff(const struct timeval &startTime)
-// {
-//     struct timeval endTime;
-//     gettimeofday(&endTime, NULL);
-//     return TimeDiff(startTime, endTime);
-// }
-
-//bool verbose = false;
-
-
 int _zisk_main(int argc, char *argv[])
 {
-    // if (argc > 1)
-    // {
-    //     if (strcmp(argv[1], "-v") == 0)
-    //     {
-    //         verbose = true;
-    //     } 
-    // }
-
-    /*********/
-    /* float */
-    /*********/
-
-    //printf("float:\n");
-    // uint64_t fregs[100];
-    // *(double *)&fregs[1] = 1.7;
-    //fregs[1] = (uint64_t)1.7;
-    //*(double *)&fregs[2] = 2.4;
-    //fregs[2] = (uint64_t)2.4;
+    // f3 = f1 + f2
+    fregs[1] = F64_ONE;
+    fregs[2] = F64_ONE;
+    uint64_t inst = 0x022081D3; // fadd.d f1, f2, f3
+    *(uint64_t *)FREG_INST = inst;
     zisk_float();
-    // printf("  f1 = %lf\n", *(double *)&fregs[1]);
-    // printf("  f2 = %lf\n", *(double *)&fregs[2]);
-    // printf("  f3 = %lf\n", *(double *)&fregs[3]);
-
-    // To compile with rv64imacfd = rv64g
-    // double a = 1.7;
-    // double b = 2.4;
-    // double c = a + b; // calls fadd.d
-    // uint64_t d = *(uint64_t *)&c;
-    // printf("d=%lx\n", d);
-
     return 0;
 }
 
