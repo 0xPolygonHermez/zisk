58154f3349fe3a60b462b0adbe1aed647e22c23d
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-09-29 10:08:08 +0000

    added duration in simulated job finished prompt

--- distributed/crates/coordinator/src/coordinator.rs
+++ distributed/crates/coordinator/src/coordinator.rs
@@ -1192,11 +1192,9 @@ impl Coordinator {
         // Reset worker statuses back to Idle
         self.workers_pool.mark_workers_with_state(&assigned_workers, WorkerState::Idle).await?;
 
-        info!(
-            "Completed simulated job {} and freed {} workers",
-            job.job_id,
-            assigned_workers.len()
-        );
+        let duration = Duration::from_millis(job.duration_ms.unwrap());
+
+        info!("[Simulated Job Finished] {} (duration: {:.3}s)", job.job_id, duration.as_secs_f32());
 
         Ok(())
     }
