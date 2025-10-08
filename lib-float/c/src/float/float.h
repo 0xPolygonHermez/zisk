7050ab47d767a0a657a437b6489afffe9ab90a90
fractasy <fractasy@gmail.com>
2025-10-08 14:58:05 +0200

    Move from const to defines in float.h

--- lib-float/c/src/float/float.h
+++ lib-float/c/src/float/float.h
@@ -10,15 +10,15 @@ extern "C" {
 #define uint64_t unsigned long long
 
 // System address where the floating-point registers are mapped
-const uint64_t SYS_ADDR = 0xa0000000;
-const uint64_t REG_FIRST = SYS_ADDR;
-const uint64_t FREG_OFFSET = 40;
-const uint64_t FREG_FIRST = SYS_ADDR + FREG_OFFSET * 8;
-const uint64_t FREG_F0 = FREG_FIRST;
-const uint64_t FREG_INST = FREG_FIRST + 33 * 8; // Floating-point instruction register (finst)
-const uint64_t FREG_X0 = FREG_FIRST + 35 * 8; // Integer register backup for floating-point instructions (fX0)
-const uint64_t CSR_ADDR = SYS_ADDR + 0x8000;
-const uint64_t FREG_CSR = CSR_ADDR + 3 * 8;
+#define SYS_ADDR 0xa0000000
+#define REG_FIRST SYS_ADDR
+#define FREG_OFFSET 40
+#define FREG_FIRST (SYS_ADDR + FREG_OFFSET * 8)
+#define FREG_F0 FREG_FIRST
+#define FREG_INST (FREG_FIRST + 33 * 8) // Floating-point instruction register (finst)
+#define FREG_X0 (FREG_FIRST + 35 * 8) // Integer register backup for floating-point instructions (fX0)
+#define CSR_ADDR (SYS_ADDR + 0x8000)
+#define FREG_CSR (CSR_ADDR + 3 * 8)
 
 // Array-like access to floating-point registers and control/status register: fregs[3], etc
 #define fregs ((volatile uint64_t *)FREG_F0)
@@ -26,32 +26,62 @@ const uint64_t FREG_CSR = CSR_ADDR + 3 * 8;
 #define fcsr (*(volatile uint32_t *)FREG_CSR)
 
 // Sign, exponent and mantissa masks for single and double precision floats
-const uint64_t F64_SIGN_BIT_MASK  = 0x8000000000000000;
-const uint64_t F64_EXPONENT_MASK  = 0x7FF0000000000000;
-const uint64_t F64_MANTISSA_MASK  = 0x000FFFFFFFFFFFFF;
-const uint64_t F64_QUIET_NAN_MASK = 0x0008000000000000;
-const uint64_t F32_SIGN_BIT_MASK  = 0xFFFFFFFF80000000;
-const uint64_t F32_EXPONENT_MASK  =         0x7F800000;
-const uint64_t F32_MANTISSA_MASK  =         0x007FFFFF;
-const uint64_t F32_QUIET_NAN_MASK =         0x00400000;
+
+/* IEEE 754 DoubleFloating Point Representation:
+
+MSB                                                                         LSB
++-----+------------------------+---------------------------------------------------+
+|  1  |           11           |                      52                           |
++-----+------------------------+---------------------------------------------------+
+|Sign |        Exponent        |                   Fraction                        |
+|     |        (E - 1023)      |               (Significand - 1)                   |
++-----+------------------------+---------------------------------------------------+
+  63      62                52   51                                               0 
+  
+  Value = (-1)^sign × 1.fraction × 2^(exponent - 1023)    ~15-16 decimal digits
+*/
+
+#define F64_SIGN_BIT_MASK  0x8000000000000000
+#define F64_EXPONENT_MASK  0x7FF0000000000000
+#define F64_MANTISSA_MASK  0x000FFFFFFFFFFFFF
+#define F64_QUIET_NAN_MASK 0x0008000000000000
+
+/* IEEE 754 Floating Point Representation:
+
+MSB                                                         LSB
++-----+------------------------+-------------------------------+
+|  1  |           8            |              23               |
++-----+------------------------+-------------------------------+
+|Sign |        Exponent        |           Fraction            |
+|     |        (E - 127)       |       (Significand - 1)       |
++-----+------------------------+-------------------------------+
+  31      30                23   22                           0
+  
+  Value = (-1)^sign × 1.fraction × 2^(exponent - 127)     ~7 decimal digits
+*/
+  
+#define F32_SIGN_BIT_MASK  0xFFFFFFFF80000000
+#define F32_EXPONENT_MASK  0x7F800000
+#define F32_MANTISSA_MASK  0x007FFFFF
+#define F32_QUIET_NAN_MASK 0x00400000
 
 // Common float values in IEEE 754 format
-const uint64_t F64_PLUS_ZERO      = 0x0000000000000000;
-const uint64_t F64_MINUS_ZERO     = 0x8000000000000000;
-const uint64_t F64_PLUS_ONE       = 0x3FF0000000000000;
-const uint64_t F64_MINUS_ONE      = 0xBFF0000000000000;
-const uint64_t F64_PLUS_INFINITE  = 0x7FF0000000000000;
-const uint64_t F64_MINUS_INFINITE = 0xFFF0000000000000;
-const uint64_t F64_QUIET_NAN      = 0x7FF8000000000000;
-const uint64_t F64_SIGNALING_NAN  = 0x7FFC000000000000;
-const uint32_t F32_PLUS_ZERO      =         0x00000000;
-const uint32_t F32_MINUS_ZERO     =         0x80000000;
-const uint32_t F32_PLUS_ONE       =         0x3F800000;
-const uint32_t F32_MINUS_ONE      =         0xBF800000;
-const uint32_t F32_MINUS_INFINITE =         0xFF800000;
-const uint32_t F32_PLUS_INFINITE  =         0x7F800000;
-const uint32_t F32_QUIET_NAN      =         0x7FC00000;
-const uint32_t F32_SIGNALING_NAN  =         0x7FE00000;
+#define F64_PLUS_ZERO      0x0000000000000000
+#define F64_MINUS_ZERO     0x8000000000000000
+#define F64_PLUS_ONE       0x3FF0000000000000
+#define F64_MINUS_ONE      0xBFF0000000000000
+#define F64_PLUS_INFINITE  0x7FF0000000000000
+#define F64_MINUS_INFINITE 0xFFF0000000000000
+#define F64_QUIET_NAN      0x7FF8000000000000
+#define F64_SIGNALING_NAN  0x7FFC000000000000
+#define F32_PLUS_ZERO      0x00000000
+#define F32_MINUS_ZERO     0x80000000
+#define F32_PLUS_ONE       0x3F800000
+#define F32_MINUS_ONE      0xBF800000
+#define F32_MINUS_INFINITE 0xFF800000
+#define F32_PLUS_INFINITE  0x7F800000
+#define F32_QUIET_NAN      0x7FC00000
+#define F32_SIGNALING_NAN  0x7FE00000
 
 // Negate a float by flipping its sign bit(s)
 #define F64_NEGATE(x) ( (x) ^ F64_SIGN_BIT_MASK )
