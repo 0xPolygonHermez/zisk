d60eac11e4651a02365739210e0e814a70d73a39
agnusmor <agnusmor@gmail.com>
2025-11-05 13:38:08 +0100

    Refactor ELF diagnostic test to handle empty input case

--- tools/test-env/test_diagnostic.sh
+++ tools/test-env/test_diagnostic.sh
@@ -6,7 +6,7 @@ main() {
     info "▶️  Running $(basename "$0") script..."
 
     ELF_FILE="zisk-programs/diagnostic/elf/diagnostic.elf"
-    INPUTS_PATH="pessimistic-proof/inputs"
+    DIAGNOSTIC_INPUTS="empty"
     test_elf "${ELF_FILE}" "${INPUTS_PATH}" "DIAGNOSTIC_INPUTS" "DIAGNOSTIC_DISTRIBUTED_INPUTS" "ELF Diagnostic" || return 1
 }
 
