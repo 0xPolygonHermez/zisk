6cd4d573fc7d6433dea712b9725f88f234a979e0
Fran Segovia <agnusmor@gmail.com>
2026-01-22 04:51:12 +0000

    Refactor hint definitions

--- ziskos/entrypoint/src/hints/bls12_381.rs
+++ ziskos/entrypoint/src/hints/bls12_381.rs
@@ -1,13 +1,13 @@
 crate::hints::macros::define_hint! {
     bls12_381_g1_add => {
         hint_id: 0x0400,
-        params: (a: 96, b: 96),
+        params: (ret: 96),
     }
 }
 
 crate::hints::macros::define_hint! {
     bls12_381_g2_add => {
         hint_id: 0x0405,
-        params: (a: 192, b: 192),
+        params: (ret: 192),
     }
 }
--- ziskos/entrypoint/src/hints/bn254.rs
+++ ziskos/entrypoint/src/hints/bn254.rs
@@ -1,13 +1,13 @@
 crate::hints::macros::define_hint! {
     bn254_g1_add => {
         hint_id: 0x0200,
-        params: (p1: 64, p2: 64),
+        params: (result: 64),
     }
 }
 
 crate::hints::macros::define_hint! {
     bn254_g1_mul => {
         hint_id: 0x0201,
-        params: (point: 64, scalar: 32),
+        params: (result: 64),
     }
 }
