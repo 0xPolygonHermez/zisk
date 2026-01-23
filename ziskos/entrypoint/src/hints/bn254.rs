e55fb15511bce76a55436d49a6a70d8f8f256493
Fran Segovia <agnusmor@gmail.com>
2026-01-23 15:53:28 +0000

    Fix function signature for hints in bn254 and secp256k1

--- ziskos/entrypoint/src/hints/bn254.rs
+++ ziskos/entrypoint/src/hints/bn254.rs
@@ -24,10 +24,10 @@ crate::hints::macros::define_hint! {
 // BN254 pairing check hint
 // Hint data layout: [num_pairs: 8 bytes][g1_point_1: 64 bytes][g2_point_1: 128 bytes]...[g1_point_n: 64 bytes][g2_point_n: 128 bytes]
 #[no_mangle]
-pub unsafe extern "C" fn hint_bn254_pairing_check(g1_ptrs: *const *const u8, g2_ptrs: *const *const u8, num_pairs: usize) {
+pub unsafe extern "C" fn hint_bn254_pairing_check(pairs: *const u8, num_pairs: usize) {
     let mut hint = Hint::default();
 
-    let total_len: u64 = num_pairs as u64 * (64 + 128);
+    let total_len: u64 = 8 + (num_pairs as u64 * (64 + 128));
     assert!(
         total_len as usize <= MAX_HINT_DATA_LEN,
         "bn254_pairing_check hint data length exceeds MAX_HINT_DATA_LEN"
@@ -42,11 +42,10 @@ pub unsafe extern "C" fn hint_bn254_pairing_check(g1_ptrs: *const *const u8, g2_
     }
 
     for i in 0..num_pairs {
-        let g1_ptr = *g1_ptrs.add(i);
-        let g2_ptr = *g2_ptrs.add(i);
+        let pair_ptr = pairs.add(i * 64 + 128);
 
-        let g1_bytes: &[u8; 64] = &*(g1_ptr as *const [u8; 64]);
-        let g2_bytes: &[u8; 128] = &*(g2_ptr as *const [u8; 128]);
+        let g1_bytes: &[u8; 64] = &*(pair_ptr as *const [u8; 64]);
+        let g2_bytes: &[u8; 128] = &*(pair_ptr.add(64) as *const [u8; 128]);
 
         let pair = concat_hint_bytes!(64 + 128; g1_bytes, g2_bytes);
 
--- ziskos/entrypoint/src/hints/secp256k1.rs
+++ ziskos/entrypoint/src/hints/secp256k1.rs
@@ -3,12 +3,13 @@ use crate::hints::{HINT_QUEUE, hint::Hint, macros::{concat_hint_bytes, register_
 const SECP256K1_ECRECOVER_HINT_ID: u32 = 0x0300;
 
 #[no_mangle]
-pub unsafe extern "C" fn hint_secp256k1_ecrecover(sig: *const u8, recid: u8, msg: *const u8) {
+pub unsafe extern "C" fn hint_secp256k1_ecrecover(sig: *const u8, recid: u8, msg: *const u8, require_low_s: bool) {
     let sig_bytes: &[u8; 64] = &*(sig as *const [u8; 64]);
     let recid_bytes: &[u8; 8] = &((recid as u64).to_le_bytes());
     let msg_bytes: &[u8; 32] = &*(msg as *const [u8; 32]);
+    let require_low_s_bytes: &[u8; 8] = &((require_low_s as u64).to_le_bytes());
 
-    let slice_bytes = concat_hint_bytes!(64 + 8 + 32; sig_bytes, recid_bytes, msg_bytes);
+    let slice_bytes = concat_hint_bytes!(64 + 8 + 32 + 8; sig_bytes, recid_bytes, msg_bytes, require_low_s_bytes);
 
     HINT_QUEUE.push(
         Hint::new(SECP256K1_ECRECOVER_HINT_ID, &slice_bytes, slice_bytes.len(), false)
