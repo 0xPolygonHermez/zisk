210969423126ac9f00abd8725963ba17d81a97a1
Fran Segovia <agnusmor@gmail.com>
2026-01-27 13:09:35 +0000

    Add hints for FP to G1 and FP2 to G2 conversions

--- ziskos/entrypoint/src/hints/bls12_381.rs
+++ ziskos/entrypoint/src/hints/bls12_381.rs
@@ -5,6 +5,8 @@ const BLS12_381_G1_MSM_HINT_ID: u32 = 0x0401;
 const BLS12_381_G2_ADD_HINT_ID: u32 = 0x0405;
 const BLS12_381_G2_MSM_HINT_ID: u32 = 0x0406;
 const BLS12_381_PAIRING_CHECK_HINT_ID: u32 = 0x040A;
+const BLS12_381_FP_TO_G1_HINT_ID: u32 = 0x0410;
+const BLS12_381_FP2_TO_G2_HINT_ID: u32 = 0x0411;
 
 define_hint! {
     bls12_381_g1_add => {
@@ -42,6 +44,22 @@ define_hint_pairs! {
     bls12_381_pairing_check => {
         hint_id: BLS12_381_PAIRING_CHECK_HINT_ID,
         pair_len: 96 + 192,
+        is_result: false,
+    }
+}
+
+define_hint! {
+    bls12_381_fp_to_g1 => {
+        hint_id: BLS12_381_FP_TO_G1_HINT_ID,
+        params: (fp: 48),
+        is_result: false,
+    }
+}
+
+define_hint! {
+    bls12_381_fp2_to_g2 => {
+        hint_id: BLS12_381_FP2_TO_G2_HINT_ID,
+        params: (fp2: 96),
         is_result: false,
     }
 }
