75210e3deaf02f4acb798108cb7e8efaacd63637
RogerTaule <roger.taulee@gmail.com>
2026-02-06 12:57:49 +0000

    Extending check setup with recursive final

--- cli/src/commands/check_setup.rs
+++ cli/src/commands/check_setup.rs
@@ -1,5 +1,5 @@
 // extern crate env_logger;
-use crate::commands::get_proving_key;
+use crate::commands::{get_proving_key, get_proving_key_snark};
 use anyhow::Result;
 use clap::Parser;
 use colored::Colorize;
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 
 use fields::Goldilocks;
 
-use proofman::ProofMan;
+use proofman::{check_setup_snark, ProofMan};
 use proofman_common::initialize_logger;
 
 #[derive(Parser)]
@@ -18,8 +18,15 @@ pub struct ZiskCheckSetup {
     #[clap(short = 'k', long)]
     pub proving_key: Option<PathBuf>,
 
+    /// Setup folder path
+    #[clap(short = 'w', long)]
+    pub proving_key_snark: Option<PathBuf>,
+
     #[clap(short = 'a', long, default_value_t = false)]
     pub aggregation: bool,
+
+    #[clap(short = 's', long, default_value_t = false)]
+    pub snark: bool,
 
     /// Verbosity (-v, -vv)
     #[arg(short, long, action = clap::ArgAction::Count, help = "Increase verbosity level")]
@@ -39,6 +46,14 @@ impl ZiskCheckSetup {
             self.verbose.into(),
         )
         .map_err(|e| anyhow::anyhow!("Error checking setup: {}", e))?;
+
+        if self.snark {
+            check_setup_snark::<Goldilocks>(
+                &get_proving_key_snark(self.proving_key_snark.as_ref()),
+                self.verbose.into(),
+            )
+            .map_err(|e| anyhow::anyhow!("Error checking setup snark: {}", e))?
+        }
 
         Ok(())
     }
