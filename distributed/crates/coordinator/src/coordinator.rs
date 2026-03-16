45e20394fec64c96a18f2da3b2cef1d4f36b94c5
RogerTaule <roger.taulee@gmail.com>
2026-03-16 14:33:39 +0000

    Adding instance logs

--- distributed/crates/coordinator/src/coordinator.rs
+++ distributed/crates/coordinator/src/coordinator.rs
@@ -2277,7 +2277,7 @@ impl Coordinator {
         let s = n.to_string();
         let mut result = String::new();
         let len = s.len();
-        
+
         for (i, c) in s.chars().enumerate() {
             if i > 0 && (len - i) % 3 == 0 {
                 result.push('.');
