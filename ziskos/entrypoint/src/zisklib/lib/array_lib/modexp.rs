691d6d745cc7db490366f0196b2dff30c8c0f86f
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2026-04-09 09:06:02 +0000

    Adding misssing assert in modexp

--- ziskos/entrypoint/src/zisklib/lib/array_lib/modexp.rs
+++ ziskos/entrypoint/src/zisklib/lib/array_lib/modexp.rs
@@ -111,6 +111,7 @@ fn modexp_short(
         #[cfg(feature = "hints")]
         hints,
     );
+    assert!(len > 0 && bits[len - 1] == 1, "Exponent must be non-zero");
 
     // We should recompose the exponent from bits to verify correctness
     let mut rec_exp = vec![0u64; len_e];
@@ -188,6 +189,7 @@ fn modexp_long(
         #[cfg(feature = "hints")]
         hints,
     );
+    assert!(len > 0 && bits[len - 1] == 1, "Exponent must be non-zero");
 
     // We should recompose the exponent from bits to verify correctness
     let mut rec_exp = vec![0u64; len_e];
