223d48ef0466ee690e16dea55f9080342a9c7dd6
Marcin Bugaj <mm.bugaj@gmail.com>
2026-05-14 17:17:16 +0200

    Enable building natively - with std

--- ziskos-staticlib/src/lib.rs
+++ ziskos-staticlib/src/lib.rs
@@ -6,18 +6,13 @@
 // Re-exporting the public interface ensures those symbols are bundled into the
 // archive.  The #[panic_handler] is required by staticlib but not rlib targets.
 
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
-pub use ziskos::zkvm_init;
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
-pub use ziskos::zkvm_deinit;
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
-pub use ziskos::zisklib::zkvm_io::read_input;
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
-pub use ziskos::zisklib::zkvm_io::write_output;
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
 pub use ziskos::zisklib::zkvm_accelerators::*;
+pub use ziskos::zisklib::zkvm_io::read_input;
+pub use ziskos::zisklib::zkvm_io::write_output;
+pub use ziskos::zkvm_deinit;
+pub use ziskos::zkvm_init;
 
-#[cfg(all(target_os = "zkvm", target_vendor = "zisk", feature = "panic-handler"))]
+#[cfg(all(feature = "panic-handler", target_os = "zkvm", target_vendor = "zisk"))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     core::intrinsics::abort()
