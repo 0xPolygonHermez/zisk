c8bd9beaa48328ccf8134471ce703fa0e670241a
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2026-05-15 09:28:37 +0000

    Adding syscall tests

--- precompiles/big_int/src/lib.rs
+++ precompiles/big_int/src/lib.rs
@@ -15,3 +15,16 @@ pub use add256_input::*;
 pub use add256_instance::*;
 pub use add256_manager::*;
 pub use add256_planner::*;
+
+#[cfg(test)]
+mod add256_tests {
+    use test_artifacts::ELF_ADD256;
+    use zisk_common::io::ZiskStdin;
+
+    #[test]
+    fn execute_add256_tests() {
+        let stdin = ZiskStdin::new();
+
+        ELF_ADD256.run_emulation(stdin, None).expect("add256 guest emulation failed");
+    }
+}
