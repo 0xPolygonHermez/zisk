eb8c93abd570f5347b3a2cd2a03aa3e0e3f9e43c
Marcin Bugaj <mm.bugaj@gmail.com>
2026-05-28 14:22:22 +0200

    Fix more global allocator uses

--- ziskos/entrypoint/src/zisklib/lib/bigint/modexp.rs
+++ ziskos/entrypoint/src/zisklib/lib/bigint/modexp.rs
@@ -1,9 +1,6 @@
 // TODO: It can be speed up by using Montgomery multiplication but knowning that divisions are "free"
 // For ref: https://www.microsoft.com/en-us/research/wp-content/uploads/1996/01/j37acmon.pdf
 
-#[cfg(zisk_guest)]
-use crate::alloc_extern::vec::Vec;
-
 use crate::scratch_accelerators::{new_scratch_vec_filled, scratch_vec_from_slice, ScratchVec};
 
 use crate::zisklib::fcall_bin_decomp;
