d263f79203756e08a0babea6ef072c05d60d4521
RogerTaule <roger.taulee@gmail.com>
2026-06-18 14:36:44 +0000

    Version SC renamed to alfa

--- zisk-contracts/ZiskVerifier.sol
+++ zisk-contracts/ZiskVerifier.sol
@@ -11,7 +11,7 @@ contract ZiskVerifier is PlonkVerifier, IZiskVerifier {
     error InvalidProof();
 
     function VERSION() external pure returns (string memory) {
-        return "v1.0.0";
+        return "v1.0.0-alfa";
     }
 
     /// @notice Root constant as bytes32 (pre-packed to match the original uint64[4] layout)
