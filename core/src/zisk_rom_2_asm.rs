aefe4769c1ccc4510711527b888fb9b6845cf75b
fractasy <fractasy@gmail.com>
2026-06-29 16:18:57 +0200

    Optimize dynamic jump refactor

--- core/src/zisk_rom_2_asm.rs
+++ core/src/zisk_rom_2_asm.rs
@@ -7895,41 +7895,71 @@ impl ZiskRom2Asm {
 
     fn jumpt_to_dynamic_pc(ctx: &mut ZiskAsmContext, code: &mut String) {
         *code += &ctx.full_line_comment("jump to dynamic pc".to_string());
+
+        // When executing zisk without float support, there are no dynamic jumps to low addresses,
+        // so we can optimize the code by skipping the check for address range, assuming that the pc
+        // is always a high address.
+        #[cfg(not(feature = "float"))]
+        {
+            let high_address: u64 = 0x8000_0000;
+            assert!(
+                ctx.pc < FLOAT_LIB_ROM_ADDR,
+                "Dynamic jump to low address is not supported without float support"
+            );
+            *code += &format!("\tbtr {}, 31 {}\n", REG_PC, ctx.comment_str("pc -= ROM_ADDR"));
+            *code += &format!(
+                "\tlea {}, [map_pc_{:x}] {}\n",
+                REG_ADDRESS,
+                high_address,
+                ctx.comment_str(&format!("address = map[0x{:x}]", high_address))
+            );
+            *code += &format!(
+                "\tmov {}, [{} + {}*8] {}\n",
+                REG_ADDRESS,
+                REG_ADDRESS,
+                REG_PC,
+                ctx.comment_str("address = map[pc]")
+            );
+            *code += &format!("\tjmp {} {}\n", REG_ADDRESS, ctx.comment_str("jump to address"));
+        }
+
         // When executing program code, it can dynamically jump to any BIOS instruction
         // (low address) or to any program code address (high address).
         // When executing zisk float library code, it can dynamically jump to any BIOS instruction
         // (low address) or to any float library code address (high address) but not to program
         // code addresses.
-        let high_address = if ctx.pc < FLOAT_LIB_ROM_ADDR { ROM_ADDR } else { FLOAT_LIB_ROM_ADDR };
-        *code += &format!(
-            "\tmov {}, 0x{:x} {}\n",
-            REG_ADDRESS,
-            high_address,
-            ctx.comment_str("is pc a low address?")
-        );
         #[cfg(feature = "float")]
         {
+            let high_address =
+                if ctx.pc < FLOAT_LIB_ROM_ADDR { ROM_ADDR } else { FLOAT_LIB_ROM_ADDR };
+            *code += &format!(
+                "\tmov {}, 0x{:x} {}\n",
+                REG_ADDRESS,
+                high_address,
+                ctx.comment_str("is pc a low address?")
+            );
             *code += &format!("\tcmp {REG_PC}, {REG_ADDRESS}\n");
             *code += &format!("\tjb pc_{:x}_jump_to_low_address\n", ctx.pc);
-        }
-        *code +=
-            &format!("\tsub {}, {} {}\n", REG_PC, REG_ADDRESS, ctx.comment_str("pc -= ROM_ADDR"));
-        *code += &format!(
-            "\tlea {}, [map_pc_{:x}] {}\n",
-            REG_ADDRESS,
-            high_address,
-            ctx.comment_str(&format!("address = map[0x{:x}]", high_address))
-        );
-        *code += &format!(
-            "\tmov {}, [{} + {}*8] {}\n",
-            REG_ADDRESS,
-            REG_ADDRESS,
-            REG_PC,
-            ctx.comment_str("address = map[pc]")
-        );
-        *code += &format!("\tjmp {} {}\n", REG_ADDRESS, ctx.comment_str("jump to address"));
-        #[cfg(feature = "float")]
-        {
+            *code += &format!(
+                "\tsub {}, {} {}\n",
+                REG_PC,
+                REG_ADDRESS,
+                ctx.comment_str("pc -= ROM_ADDR")
+            );
+            *code += &format!(
+                "\tlea {}, [map_pc_{:x}] {}\n",
+                REG_ADDRESS,
+                high_address,
+                ctx.comment_str(&format!("address = map[0x{:x}]", high_address))
+            );
+            *code += &format!(
+                "\tmov {}, [{} + {}*8] {}\n",
+                REG_ADDRESS,
+                REG_ADDRESS,
+                REG_PC,
+                ctx.comment_str("address = map[pc]")
+            );
+            *code += &format!("\tjmp {} {}\n", REG_ADDRESS, ctx.comment_str("jump to address"));
             *code += &format!("pc_{:x}_jump_to_low_address:\n", ctx.pc);
             *code += &format!("\tsub {}, 0x1000 {}\n", REG_PC, ctx.comment_str("pc -= ROM_ENTRY"));
             *code += &format!(
