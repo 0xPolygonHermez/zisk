0b84d15d3066e1d732b467f5be3b1dfb82b4635f
RogerTaule <roger.taulee@gmail.com>
2026-09-07 11:09:27 +0000

    Removing table_airs

--- cli/src/commands/dev/program_setup.rs
+++ cli/src/commands/dev/program_setup.rs
@@ -96,7 +96,6 @@ impl ProgramSetupCmd {
             &ProofType::Basic,
             false,
             &[],
-            &[],
             gpu,
         )?);
         let setups_vadcop = Arc::new(SetupsVadcop::new(&pctx.global_info, false, false, &[], gpu)?);
